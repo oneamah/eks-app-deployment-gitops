da9d55e998d32ba38cbacd3678e36c3aa29c54e6
osamah-devops <osamah.devops@gmail.com>
2026-07-04 15:28:09 -0400

    Add Helm And Kubernetes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -85,6 +85,20 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  depends_on = [
+    aws_internet_gateway.main,
+    aws_route_table_association.public
+  ]
+}
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -102,6 +116,11 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
 }
 
 resource "aws_route_table_association" "private" {
