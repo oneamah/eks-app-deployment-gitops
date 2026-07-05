ac993fcc93c9f0e9380afecb28be68ba36196e38
osamah-devops <osamah.devops@gmail.com>
2026-07-04 22:29:19 -0400

    Add posts Application

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1486,15 +1486,15 @@ resource "kubernetes_manifest" "backend_ingress" {
       name      = "backend-ingress"
       namespace = local.backend_namespace
       annotations = {
-        "kubernetes.io/ingress.class"               = "alb"
-        "alb.ingress.kubernetes.io/group.name"      = "app-shared"
-        "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
-        "alb.ingress.kubernetes.io/target-type"     = "ip"
-        "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTP\":80},{\"HTTPS\":443}]"
-        "alb.ingress.kubernetes.io/certificate-arn" = "${aws_acm_certificate_validation.frontend[0].certificate_arn},${aws_acm_certificate_validation.backend[0].certificate_arn},${aws_acm_certificate_validation.monitoring[0].certificate_arn}"
-        "alb.ingress.kubernetes.io/ssl-redirect"    = "443"
+        "kubernetes.io/ingress.class"                = "alb"
+        "alb.ingress.kubernetes.io/group.name"       = "app-shared"
+        "alb.ingress.kubernetes.io/scheme"           = "internet-facing"
+        "alb.ingress.kubernetes.io/target-type"      = "ip"
+        "alb.ingress.kubernetes.io/listen-ports"     = "[{\"HTTP\":80},{\"HTTPS\":443}]"
+        "alb.ingress.kubernetes.io/certificate-arn"  = "${aws_acm_certificate_validation.frontend[0].certificate_arn},${aws_acm_certificate_validation.backend[0].certificate_arn},${aws_acm_certificate_validation.monitoring[0].certificate_arn}"
+        "alb.ingress.kubernetes.io/ssl-redirect"     = "443"
         "alb.ingress.kubernetes.io/healthcheck-path" = "/health"
-        "external-dns.alpha.kubernetes.io/hostname" = "api.marmil.co"
+        "external-dns.alpha.kubernetes.io/hostname"  = "api.marmil.co"
       }
     }
     spec = {
